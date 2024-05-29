67cc89ee3439098a77b271576ab16877a4cc04e5
ErnestoAli <aliernesto05@gmail.com>
2024-05-29 14:44:26 -0300

    a

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 
-        const formulario = document.getElementById('formularioContacto');
-        const nombreInput = document.getElementById('nombre');
-        const emailInput = document.getElementById('email');
-        const mensajeInput = document.getElementById('mensaje');
-        const errorNombre = document.getElementById('errorNombre');
-        const errorEmail = document.getElementById('errorEmail');
-        const errorMensaje = document.getElementById('errorMensaje');
-        const datosEnviados = document.getElementById('datosEnviados');
-        const nombreEnviado = document.getElementById('nombreEnviado');
-        const emailEnviado = document.getElementById('emailEnviado');
-        const mensajeEnviado = document.getElementById('mensajeEnviado');
         const switchers = document.querySelectorAll('.switcher')
-       
        
         switchers.forEach(item => {
              item.addEventListener('click', function() {
@@ -19,27 +7,6 @@
                 this.parentElement.classList.add('is-active')
             })
         })
-
-        formulario.addEventListener('submit', (event) => {
-            event.preventDefault();
-
-            const nombre = nombreInput.value.trim();
-            const email = emailInput.value.trim();
-            const mensaje = mensajeInput.value.trim();
-
-            let errores = false;
-
-            if (nombre === '') {
-                errorNombre.textContent = 'El nombre es obligatorio';
-                errores = true;
-            } else {
-                errorNombre.textContent = '';
-            }
-
-            if (!validarEmail(email)) {
-                errorEmail.textContent = 'Correo electrónico no válido';
-                errores = true }
-        }
         
     )
    
