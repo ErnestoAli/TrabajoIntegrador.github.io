8722523595251b3b361f1a132086cdf372cfd784
ErnestoAli <aliernesto05@gmail.com>
2024-05-31 10:11:16 -0300

    formulariop

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
+const form = document.getElementById('FormContacto');
+const resulta = document.getElementById('resultado');
 
-        const switchers = document.querySelectorAll('.switcher')
-       
-        switchers.forEach(item => {
-             item.addEventListener('click', function() {
-                switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-                this.parentElement.classList.add('is-active')
-            })
-        })
-        
-    )
-   
-    
-    
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const nombre = e.target.name.value;
+  const email = e.target.email.value;
+  const telefono = e.target.phone.value;
+  const mensaje = e.target.message.value;
+
+  if (!nombre ||!email ||!telefono ||!mensaje) {
+    resulta.innerHTML = '<p>Por favor, completa todos los campos.</p>';
+    return;
+  }
+
+  if (!/^[0-9]{10}$/.test(telefono)) {
+    resulta.innerHTML = '<p>El número de teléfono debe tener 10 dígitos.</p>';
+    return;
+  }
+
+  resulta.innerHTML = '<p>Mensaje enviado</p>';
+
+})
